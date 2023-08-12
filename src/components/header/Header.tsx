c7f16bd83cd1d0f153789cbf37ad11141c1cd4f7
BeWoler <bewoler.del@gmail.com>
2023-08-13 01:40:12 +0300

    added default structure

---
+++ src/components/header/Header.tsx
@@ -0,0 +1,10 @@
+const Header = () => {
+  return (
+    <header className="flex justify-around flex-row border-b-2 mb-8">
+      <div>Hello</div>
+      <div>TanStack Query</div>
+    </header>
+  );
+};
+
+export default Header;
